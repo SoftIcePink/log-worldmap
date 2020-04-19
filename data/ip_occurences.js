bd05b2f49429f6a463190d4f0ecd75546902c2f5
SoftIce <32594805+SoftIcePink@users.noreply.github.com>
2020-04-19 17:06:15 +0200

    First version v2020.04.19
    Things should work, minimal options, but still something.

---
+++ data/ip_occurences.js
@@ -0,0 +1,278 @@
+/* 
+  Could have done it myself, but it already exists  
+  CSV to JSON : https://www.csvjson.com/csv2json
+*/
+var logData =[
+  {
+    "IP": "106.13.24.47",
+    "Occurrences": 2
+  },
+  {
+    "IP": "109.235.25.43",
+    "Occurrences": 2
+  },
+  {
+    "IP": "112.124.42.80",
+    "Occurrences": 1
+  },
+  {
+    "IP": "109.235.25.43",
+    "Occurrences": 2
+  },
+  {
+    "IP": "117.136.26.30",
+    "Occurrences": 10
+  },
+  {
+    "IP": "118.24.97.147",
+    "Occurrences": 939
+  },
+  {
+    "IP": "119.39.46.126",
+    "Occurrences": 1
+  },
+  {
+    "IP": "123.125.114.144",
+    "Occurrences": 1
+  },
+  {
+    "IP": "123.160.174.146",
+    "Occurrences": 1
+  },
+  {
+    "IP": "124.88.112.215",
+    "Occurrences": 1
+  },
+  {
+    "IP": "128.14.133.58",
+    "Occurrences": 1
+  },
+  {
+    "IP": "128.14.134.170",
+    "Occurrences": 1
+  },
+  {
+    "IP": "138.122.20.76",
+    "Occurrences": 1
+  },
+  {
+    "IP": "138.204.132.211",
+    "Occurrences": 1
+  },
+  {
+    "IP": "14.231.97.7",
+    "Occurrences": 1
+  },
+  {
+    "IP": "141.8.183.79",
+    "Occurrences": 2
+  },
+  {
+    "IP": "159.203.76.218",
+    "Occurrences": 1
+  },
+  {
+    "IP": "162.243.130.23",
+    "Occurrences": 1
+  },
+  {
+    "IP": "167.172.97.0",
+    "Occurrences": 1
+  },
+  {
+    "IP": "172.104.242.173",
+    "Occurrences": 1
+  },
+  {
+    "IP": "175.152.109.245",
+    "Occurrences": 1
+  },
+  {
+    "IP": "175.184.165.143",
+    "Occurrences": 1
+  },
+  {
+    "IP": "176.9.193.37",
+    "Occurrences": 2
+  },
+  {
+    "IP": "178.128.6.69",
+    "Occurrences": 6
+  },
+  {
+    "IP": "182.138.158.201",
+    "Occurrences": 1
+  },
+  {
+    "IP": "183.129.159.242",
+    "Occurrences": 1
+  },
+  {
+    "IP": "184.154.47.2",
+    "Occurrences": 1
+  },
+  {
+    "IP": "187.226.248.9",
+    "Occurrences": 1
+  },
+  {
+    "IP": "190.122.159.217",
+    "Occurrences": 1
+  },
+  {
+    "IP": "190.138.5.95",
+    "Occurrences": 1
+  },
+  {
+    "IP": "190.94.150.99",
+    "Occurrences": 1
+  },
+  {
+    "IP": "191.100.8.216",
+    "Occurrences": 1
+  },
+  {
+    "IP": "2.238.139.133",
+    "Occurrences": 1
+  },
+  {
+    "IP": "201.1.87.161",
+    "Occurrences": 2
+  },
+  {
+    "IP": "212.75.249.210",
+    "Occurrences": 1
+  },
+  {
+    "IP": "213.159.213.137",
+    "Occurrences": 1
+  },
+  {
+    "IP": "219.140.116.66",
+    "Occurrences": 1
+  },
+  {
+    "IP": "220.143.85.88",
+    "Occurrences": 1
+  },
+  {
+    "IP": "221.225.77.197",
+    "Occurrences": 1
+  },
+  {
+    "IP": "222.82.60.115",
+    "Occurrences": 1
+  },
+  {
+    "IP": "223.167.74.196",
+    "Occurrences": 1
+  },
+  {
+    "IP": "24.12.69.236",
+    "Occurrences": 1
+  },
+  {
+    "IP": "31.43.152.44",
+    "Occurrences": 2
+  },
+  {
+    "IP": "36.80.37.244",
+    "Occurrences": 1
+  },
+  {
+    "IP": "37.201.222.32",
+    "Occurrences": 1
+  },
+  {
+    "IP": "42.236.10.108",
+    "Occurrences": 1
+  },
+  {
+    "IP": "42.236.10.109",
+    "Occurrences": 1
+  },
+  {
+    "IP": "46.19.227.218",
+    "Occurrences": 1
+  },
+  {
+    "IP": "46.19.227.218",
+    "Occurrences": 1
+  },
+  {
+    "IP": "54.214.64.205",
+    "Occurrences": 1
+  },
+  {
+    "IP": "54.36.148.206",
+    "Occurrences": 1
+  },
+  {
+    "IP": "54.36.149.48",
+    "Occurrences": 1
+  },
+  {
+    "IP": "54.36.150.115",
+    "Occurrences": 1
+  },
+  {
+    "IP": "54.36.150.13",
+    "Occurrences": 1
+  },
+  {
+    "IP": "54.36.150.180",
+    "Occurrences": 1
+  },
+  {
+    "IP": "54.36.150.19",
+    "Occurrences": 1
+  },
+  {
+    "IP": "60.191.52.254",
+    "Occurrences": 1
+  },
+  {
+    "IP": "60.216.141.137",
+    "Occurrences": 1
+  },
+  {
+    "IP": "61.2.153.204",
+    "Occurrences": 2
+  },
+  {
+    "IP": "62.173.148.243",
+    "Occurrences": 1
+  },
+  {
+    "IP": "62.173.149.158",
+    "Occurrences": 2
+  },
+  {
+    "IP": "62.173.149.52",
+    "Occurrences": 1
+  },
+  {
+    "IP": "71.6.232.4",
+    "Occurrences": 1
+  },
+  {
+    "IP": "77.247.110.63",
+    "Occurrences": 6
+  },
+  {
+    "IP": "83.219.136.119",
+    "Occurrences": 1
+  },
+  {
+    "IP": "89.36.51.161",
+    "Occurrences": 1
+  },
+  {
+    "IP": "91.217.79.207",
+    "Occurrences": 1
+  },
+  {
+    "IP": "91.234.102.36",
+    "Occurrences": 1
+  }
+];
---
+++ data/test.js
@@ -0,0 +1,548 @@
+var test = [
+    {
+      "IP": "106.13.24.47",
+      "Occurrences": 2,
+      "Location": "Beijing",
+      "Longitude": 116,
+      "Latitude": 39,
+      "Identity": "Beijing Baidu Netcom Science and Technology Co., Ltd."
+    },
+    {
+      "IP": "109.235.25.43",
+      "Occurrences": 2,
+      "Location": "Moscow",
+      "Longitude": 37,
+      "Latitude": 55,
+      "Identity": "SvyazService Ltd"
+    },
+    {
+      "IP": "112.124.42.80",
+      "Occurrences": 1,
+      "Location": "Hangzhou",
+      "Longitude": 120,
+      "Latitude": 30,
+      "Identity": "Hangzhou Alibaba Advertising Co.,Ltd."
+    },
+    {
+      "IP": "109.235.25.43",
+      "Occurrences": 2,
+      "Location": "Moscow",
+      "Longitude": 37,
+      "Latitude": 55,
+      "Identity": "SvyazService Ltd"
+    },
+    {
+      "IP": "117.136.26.30",
+      "Occurrences": 10,
+      "Location": "Guangzhou",
+      "Longitude": 113,
+      "Latitude": 23,
+      "Identity": "China Mobile"
+    },
+    {
+      "IP": "118.24.97.147",
+      "Occurrences": 939,
+      "Location": "Haidian",
+      "Longitude": 116,
+      "Latitude": 39,
+      "Identity": "Shenzhen Tencent Computer Systems Company Limited"
+    },
+    {
+      "IP": "119.39.46.126",
+      "Occurrences": 1,
+      "Location": "Yanqing Qu",
+      "Longitude": 115,
+      "Latitude": 40,
+      "Identity": "CHINA UNICOM China169 Backbone"
+    },
+    {
+      "IP": "123.125.114.144",
+      "Occurrences": 1,
+      "Location": "Beijing",
+      "Longitude": 116,
+      "Latitude": 39,
+      "Identity": "China Unicom Beijing Province Network"
+    },
+    {
+      "IP": "123.160.174.146",
+      "Occurrences": 1,
+      "Location": "Henan",
+      "Longitude": 113,
+      "Latitude": 34,
+      "Identity": "No.31,Jin-rong Street"
+    },
+    {
+      "IP": "124.88.112.215",
+      "Occurrences": 1,
+      "Location": "Ürümqi",
+      "Longitude": 87,
+      "Latitude": 43,
+      "Identity": "CHINA UNICOM China169 Backbone"
+    },
+    {
+      "IP": "128.14.133.58",
+      "Occurrences": 1,
+      "Location": "Georgetown",
+      "Longitude": 143,
+      "Latitude": -18,
+      "Identity": "Zenlayer Inc"
+    },
+    {
+      "IP": "128.14.134.170",
+      "Occurrences": 1,
+      "Location": "Georgetown",
+      "Longitude": 143,
+      "Latitude": -18,
+      "Identity": "Zenlayer Inc"
+    },
+    {
+      "IP": "138.122.20.76",
+      "Occurrences": 1,
+      "Location": "Irati",
+      "Longitude": -52,
+      "Latitude": -26,
+      "Identity": "RM INFORMATICA LTDA"
+    },
+    {
+      "IP": "138.204.132.211",
+      "Occurrences": 1,
+      "Location": "Campinaçu",
+      "Longitude": -48,
+      "Latitude": -13,
+      "Identity": "FIUZA INFORMÃTICA & TELECOMUNICAÃÃO LTDA ME"
+    },
+    {
+      "IP": "14.231.97.7",
+      "Occurrences": 1,
+      "Location": "Hanoi",
+      "Longitude": 105,
+      "Latitude": 21,
+      "Identity": "VNPT Corp"
+    },
+    {
+      "IP": "141.8.183.79",
+      "Occurrences": 2,
+      "Location": "Moscow",
+      "Longitude": 37,
+      "Latitude": 55,
+      "Identity": "YANDEX LLC"
+    },
+    {
+      "IP": "159.203.76.218",
+      "Occurrences": 1,
+      "Location": "Clifton",
+      "Longitude": -74,
+      "Latitude": 40,
+      "Identity": "ASN block not managed by the RIPE NCC"
+    },
+    {
+      "IP": "162.243.130.23",
+      "Occurrences": 1,
+      "Location": "San Francisco",
+      "Longitude": -122,
+      "Latitude": 37,
+      "Identity": "ASN block not managed by the RIPE NCC"
+    },
+    {
+      "IP": "167.172.97.0",
+      "Occurrences": 1,
+      "Location": "London",
+      "Longitude": 0,
+      "Latitude": 51,
+      "Identity": ""
+    },
+    {
+      "IP": "172.104.242.173",
+      "Occurrences": 1,
+      "Location": "Frankfurt",
+      "Longitude": 8,
+      "Latitude": 50,
+      "Identity": "ASN block not managed by the RIPE NCC"
+    },
+    {
+      "IP": "175.152.109.245",
+      "Occurrences": 1,
+      "Location": "Chaowai",
+      "Longitude": 116,
+      "Latitude": 39,
+      "Identity": "CHINA UNICOM China169 Backbone"
+    },
+    {
+      "IP": "175.184.165.143",
+      "Occurrences": 1,
+      "Location": "Jinrongjie",
+      "Longitude": 116,
+      "Latitude": 39,
+      "Identity": "CHINA UNICOM China169 Backbone"
+    },
+    {
+      "IP": "176.9.193.37",
+      "Occurrences": 2,
+      "Location": "Falkenstein",
+      "Longitude": 12,
+      "Latitude": 49,
+      "Identity": "Hetzner Online GmbH"
+    },
+    {
+      "IP": "178.128.6.69",
+      "Occurrences": 6,
+      "Location": "Santa Clara",
+      "Longitude": -121,
+      "Latitude": 37,
+      "Identity": "ASN block not managed by the RIPE NCC"
+    },
+    {
+      "IP": "182.138.158.201",
+      "Occurrences": 1,
+      "Location": "Zhongba",
+      "Longitude": 102,
+      "Latitude": 30,
+      "Identity": "No.31,Jin-rong Street"
+    },
+    {
+      "IP": "183.129.159.242",
+      "Occurrences": 1,
+      "Location": "Shaoxing Xian",
+      "Longitude": 120,
+      "Latitude": 30,
+      "Identity": "No.31,Jin-rong Street"
+    },
+    {
+      "IP": "184.154.47.2",
+      "Occurrences": 1,
+      "Location": "Chicago",
+      "Longitude": -87,
+      "Latitude": 41,
+      "Identity": "SingleHop LLC"
+    },
+    {
+      "IP": "187.226.248.9",
+      "Occurrences": 1,
+      "Location": "Mexico City",
+      "Longitude": -99,
+      "Latitude": 19,
+      "Identity": "Uninet S.A. de C.V."
+    },
+    {
+      "IP": "190.122.159.217",
+      "Occurrences": 1,
+      "Location": "Buenos Aires City",
+      "Longitude": -58,
+      "Latitude": -34,
+      "Identity": "Atlantica Video Cable S.A."
+    },
+    {
+      "IP": "190.138.5.95",
+      "Occurrences": 1,
+      "Location": "Buenos Aires City",
+      "Longitude": -58,
+      "Latitude": -34,
+      "Identity": "Telecom Argentina S.A."
+    },
+    {
+      "IP": "190.94.150.99",
+      "Occurrences": 1,
+      "Location": "Quito",
+      "Longitude": -78,
+      "Latitude": 0,
+      "Identity": "ETAPA EP"
+    },
+    {
+      "IP": "191.100.8.216",
+      "Occurrences": 1,
+      "Location": "Quito",
+      "Longitude": -78,
+      "Latitude": 0,
+      "Identity": "ETAPA EP"
+    },
+    {
+      "IP": "2.238.139.133",
+      "Occurrences": 1,
+      "Location": "Bologna",
+      "Longitude": 11,
+      "Latitude": 44,
+      "Identity": "Fastweb SpA"
+    },
+    {
+      "IP": "201.1.87.161",
+      "Occurrences": 2,
+      "Location": "Sao Paulo",
+      "Longitude": -46,
+      "Latitude": -23,
+      "Identity": "TELEFÃNICA BRASIL S.A"
+    },
+    {
+      "IP": "212.75.249.210",
+      "Occurrences": 1,
+      "Location": "Saint Petersburg",
+      "Longitude": 30,
+      "Latitude": 59,
+      "Identity": "JSC ER-Telecom Holding"
+    },
+    {
+      "IP": "213.159.213.137",
+      "Occurrences": 1,
+      "Location": "Irkutsk",
+      "Longitude": 104,
+      "Latitude": 52,
+      "Identity": "JSC The First"
+    },
+    {
+      "IP": "219.140.116.66",
+      "Occurrences": 1,
+      "Location": "Macau",
+      "Longitude": 113,
+      "Latitude": 22,
+      "Identity": "No.31,Jin-rong Street"
+    },
+    {
+      "IP": "220.143.85.88",
+      "Occurrences": 1,
+      "Location": "Taipei City",
+      "Longitude": 121,
+      "Latitude": 25,
+      "Identity": "Chunghwa Telecom Co., Ltd."
+    },
+    {
+      "IP": "221.225.77.197",
+      "Occurrences": 1,
+      "Location": "Xuzhou",
+      "Longitude": 117,
+      "Latitude": 34,
+      "Identity": "No.31,Jin-rong Street"
+    },
+    {
+      "IP": "222.82.60.115",
+      "Occurrences": 1,
+      "Location": "Urumqi",
+      "Longitude": 87,
+      "Latitude": 43,
+      "Identity": "No.31,Jin-rong Street"
+    },
+    {
+      "IP": "223.167.74.196",
+      "Occurrences": 1,
+      "Location": "Shanghai",
+      "Longitude": 121,
+      "Latitude": 31,
+      "Identity": "China Unicom Shanghai network"
+    },
+    {
+      "IP": "24.12.69.236",
+      "Occurrences": 1,
+      "Location": "Glendale Heights",
+      "Longitude": -88,
+      "Latitude": 41,
+      "Identity": "Comcast Cable Communications, LLC"
+    },
+    {
+      "IP": "31.43.152.44",
+      "Occurrences": 2,
+      "Location": "Kyiv",
+      "Longitude": 30,
+      "Latitude": 50,
+      "Identity": "Corbina Telecom Llc."
+    },
+    {
+      "IP": "36.80.37.244",
+      "Occurrences": 1,
+      "Location": "Jakarta Pusat",
+      "Longitude": 106,
+      "Latitude": -6,
+      "Identity": "Asia Pacific Network Information Centre"
+    },
+    {
+      "IP": "37.201.222.32",
+      "Occurrences": 1,
+      "Location": "Frankfurt",
+      "Longitude": 8,
+      "Latitude": 50,
+      "Identity": "Liberty Global B.V."
+    },
+    {
+      "IP": "42.236.10.108",
+      "Occurrences": 1,
+      "Location": "luohe shi",
+      "Longitude": 113,
+      "Latitude": 33,
+      "Identity": "CHINA UNICOM China169 Backbone"
+    },
+    {
+      "IP": "42.236.10.109",
+      "Occurrences": 1,
+      "Location": "luohe shi",
+      "Longitude": 113,
+      "Latitude": 33,
+      "Identity": "CHINA UNICOM China169 Backbone"
+    },
+    {
+      "IP": "46.19.227.218",
+      "Occurrences": 1,
+      "Location": "Ljubljana",
+      "Longitude": 14,
+      "Latitude": 46,
+      "Identity": "Kujtesa Net Sh.p.k."
+    },
+    {
+      "IP": "46.19.227.218",
+      "Occurrences": 1,
+      "Location": "Ljubljana",
+      "Longitude": 14,
+      "Latitude": 46,
+      "Identity": "Kujtesa Net Sh.p.k."
+    },
+    {
+      "IP": "54.214.64.205",
+      "Occurrences": 1,
+      "Location": "",
+      "Longitude": -123,
+      "Latitude": 44,
+      "Identity": "Amazon Technologies Inc."
+    },
+    {
+      "IP": "54.36.148.206",
+      "Occurrences": 1,
+      "Location": "Roubaix",
+      "Longitude": 3,
+      "Latitude": 50,
+      "Identity": "OVH SAS"
+    },
+    {
+      "IP": "54.36.149.48",
+      "Occurrences": 1,
+      "Location": "Roubaix",
+      "Longitude": 3,
+      "Latitude": 50,
+      "Identity": "OVH SAS"
+    },
+    {
+      "IP": "54.36.150.115",
+      "Occurrences": 1,
+      "Location": "Gravelines",
+      "Longitude": 2,
+      "Latitude": 50,
+      "Identity": "OVH SAS"
+    },
+    {
+      "IP": "54.36.150.13",
+      "Occurrences": 1,
+      "Location": "Gravelines",
+      "Longitude": 2,
+      "Latitude": 50,
+      "Identity": "OVH SAS"
+    },
+    {
+      "IP": "54.36.150.180",
+      "Occurrences": 1,
+      "Location": "Gravelines",
+      "Longitude": 2,
+      "Latitude": 50,
+      "Identity": "OVH SAS"
+    },
+    {
+      "IP": "54.36.150.19",
+      "Occurrences": 1,
+      "Location": "Gravelines",
+      "Longitude": 2,
+      "Latitude": 50,
+      "Identity": "OVH SAS"
+    },
+    {
+      "IP": "60.191.52.254",
+      "Occurrences": 1,
+      "Location": "Shaoxing Xian",
+      "Longitude": 120,
+      "Latitude": 29,
+      "Identity": "No.31,Jin-rong Street"
+    },
+    {
+      "IP": "60.216.141.137",
+      "Occurrences": 1,
+      "Location": "Qingdao",
+      "Longitude": 120,
+      "Latitude": 36,
+      "Identity": "CHINA UNICOM China169 Backbone"
+    },
+    {
+      "IP": "61.2.153.204",
+      "Occurrences": 2,
+      "Location": "Bengaluru",
+      "Longitude": 77,
+      "Latitude": 20,
+      "Identity": "Bharat Sanchar Nigam Ltd"
+    },
+    {
+      "IP": "62.173.148.243",
+      "Occurrences": 1,
+      "Location": "Moscow",
+      "Longitude": 37,
+      "Latitude": 55,
+      "Identity": "Internet-Cosmos LLC"
+    },
+    {
+      "IP": "62.173.149.158",
+      "Occurrences": 2,
+      "Location": "Moscow",
+      "Longitude": 37,
+      "Latitude": 55,
+      "Identity": "Internet-Cosmos LLC"
+    },
+    {
+      "IP": "62.173.149.52",
+      "Occurrences": 1,
+      "Location": "Moscow",
+      "Longitude": 37,
+      "Latitude": 55,
+      "Identity": "Internet-Cosmos LLC"
+    },
+    {
+      "IP": "71.6.232.4",
+      "Occurrences": 1,
+      "Location": "San Diego",
+      "Longitude": -117,
+      "Latitude": 32,
+      "Identity": "CariNet, Inc."
+    },
+    {
+      "IP": "77.247.110.63",
+      "Occurrences": 6,
+      "Location": "Tallinn",
+      "Longitude": 24,
+      "Latitude": 59,
+      "Identity": "VITOX TELECOM"
+    },
+    {
+      "IP": "83.219.136.119",
+      "Occurrences": 1,
+      "Location": "Kaliningrad",
+      "Longitude": 20,
+      "Latitude": 54,
+      "Identity": "TIS Dialog LLC"
+    },
+    {
+      "IP": "89.36.51.161",
+      "Occurrences": 1,
+      "Location": "Iran",
+      "Longitude": 48,
+      "Latitude": 34,
+      "Identity": "Iran Telecommunication Company PJS"
+    },
+    {
+      "IP": "91.217.79.207",
+      "Occurrences": 1,
+      "Location": "Lipetsk",
+      "Longitude": 39,
+      "Latitude": 52,
+      "Identity": "EKMA IS LTD"
+    },
+    {
+      "IP": "91.234.102.36",
+      "Occurrences": 1,
+      "Location": "Piekary Śląskie",
+      "Longitude": 18,
+      "Latitude": 50,
+      "Identity": "IPI Vision Sp. z o.o."
+    }
+  ];
+          
+  
